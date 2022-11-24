bcd2ba6195c7942d8a9341b002fe9bc790f7c4de
Neo Lee <brandnewx@gmail.com>
2022-11-24 21:15:50 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,20 +4,22 @@ echo "building brandnewx/ubuntu:cuda-pytorch..."
 docker build -t brandnewx/ubuntu22:cuda-pytorch ./services/ubuntu22-cuda-pytorch/
 [[ $? != 0 ]] && echo "Error!" && return 13
 
-# set to compile xformers for different nvidia computes (see https://developer.nvidia.com/cuda-gpus#compute)
-smi=$(nvidia-smi)
-if [[ $smi == *"A100"* ]]; then
-  echo "building brandnewx/xformers:a100 ..."
-  docker build -t ubuntu22:xformers:a100 -f ./services/xformers/Dockerfile.a100 ./services/xformers
-elif [[ $smi == *"T4"* ]]; then
-  echo "building brandnewx/xformers:t4 ..."
-  docker build -t brandnewx/xformers:t4 -f ./services/xformers/Dockerfile.t4 ./services/xformers
-else
-  echo "skipped building xformers as current gpu is not supported!"
-fi
+docker build -t brandnewx/xformers ./services/xformers
 [[ $? != 0 ]] && echo "Error!" && return 14
+
+## set to compile xformers for different nvidia computes (see https://developer.nvidia.com/cuda-gpus#compute)
+#smi=$(nvidia-smi)
+#if [[ $smi == *"A100"* ]]; then
+#  echo "building brandnewx/xformers:a100 ..."
+#  docker build -t brandnewx:xformers:a100 -f ./services/xformers/Dockerfile.a100 ./services/xformers
+#elif [[ $smi == *"T4"* ]]; then
+#  echo "building brandnewx/xformers:t4 ..."
+#  docker build -t brandnewx/xformers:t4 -f ./services/xformers/Dockerfile.t4 ./services/xformers
+#else
+#  echo "skipped building xformers as current gpu is not supported!"
+#fi
+#[[ $? != 0 ]] && echo "Error!" && return 14
 
 echo "building automatic1111:t4"
 docker build -t automatic1111:t4 ./services/AUTOMATIC1111/
-
-[[ $? != 0 ]] && echo "Error!" && return 14
+[[ $? != 0 ]] && echo "Error!" && return 15
