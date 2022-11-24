ad2e3493d9e0313b9ffb36b6ef15c2e738c62086
Neo Lee <brandnewx@gmail.com>
2022-11-24 19:58:08 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!bin/bash -xe
 
-echo "building ubuntu:cuda-pytorch..."
-docker build -t ubuntu22:cuda-pytorch ./services/cuda-pytorch-ubuntu22/
+echo "building brandnewx/ubuntu:cuda-pytorch..."
+docker build -t brandnewx/ubuntu22:cuda-pytorch ./services/cuda-pytorch-ubuntu22/
 [[ $? != 0 ]] && echo "Error!" && return 13
 
 # set to compile xformers for different nvidia computes (see https://developer.nvidia.com/cuda-gpus#compute)
 smi=$(nvidia-smi)
 if [[ $smi == *"A100"* ]]; then
-  echo "building ubuntu22:cuda-pytorch-xformers-a100..."
-  docker build --build-arg TORCH_CUDA_ARCH_LIST="8.0+PTX" -t ubuntu22:cuda-pytorch-xformers-a100 ./services/cuda-pytorch-ubuntu22/xformers/ # && docker push brandnewx/ubuntu22:cuda-pytorch-xformers-a100
+  echo "building brandnewx/xformers:a100 ..."
+  docker build -t ubuntu22:xformers-a100 ./services/xformers/Dockerfile.a100
 elif [[ $smi == *"T4"* ]]; then
-  echo "building ubuntu22:cuda-pytorch-xformers-t4"
-  docker build --build-arg TORCH_CUDA_ARCH_LIST="7.5+PTX" -t ubuntu22:cuda-pytorch-xformers-t4 ./services/cuda-pytorch-ubuntu22/xformers/ # && docker push brandnewx/ubuntu22:cuda-pytorch-xformers-t4
+  echo "building brandnewx/xformers:t4 ..."
+  docker build -t brandnewx/xformers:t4 ./services/xformers/Dockerfile.t4
 else
   echo "skipped building xformers as current gpu is not supported!"
 fi
