b4b5b00d0724011c2c75ac7e3a0df38e4c1c6d1e
Sopheap Ly <brandnewx@gmail.com>
2022-11-26 03:56:13 +0000

    dsb

--- services/dreambooth/entrypoint.sh
+++ services/dreambooth/entrypoint.sh
@@ -2,54 +2,58 @@
 
 set -Eeuo pipefail
 
-[[ -z "$INSTANCE_DIR" ]] && echo "INSTANCE_DIR not specified" && return 10 
-[[ -z "$TEXTENCODER_STEPS" ]] && echo "TEXTENCODER_STEPS not specified" && return 11
-[[ -z "$SESSION_NAME" ]] && echo "SESSION_NAME not specified" && return 12
+[[ -z "$INSTANCE_DIR" ]] && echo "INSTANCE_DIR not specified" && return 110 
+[[ -z "$TEXTENCODER_STEPS" ]] && echo "TEXTENCODER_STEPS not specified" && return 120
+[[ -z "$MODEL_NAME" ]] && echo "MODEL_NAME not specified" && return 130
+[[ -z "$OUTPUT_DIR" ]] && echo "OUTPUT_DIR not specified" && return 140
 
-SESSION_DIR="/sessions/${SESSION_NAME}"
-WORKING_MODEL_DIR="${SESSION_DIR}/working/model"
-OUTPUT_DIR="${SESSION_DIR}/output"
-MODEL_FILE="${WORKING_MODEL_DIR}/unet/diffusion_pytorch_model.bin"
-MODEL_DOWNLOADED="${WORKING_MODEL_DIR}/downloaded.ckpt"
+mkdir -p "$OUTPUT_DIR"
+SESSION_DIR="${OUTPUT_DIR}/${MODEL_NAME}"
+SESSION_MODEL_DIR="${SESSION_DIR}/model"
+UNET_FILE="${SESSION_MODEL_DIR}/unet/diffusion_pytorch_model.bin"
+MODEL_DOWNLOADED="${SESSION_MODEL_DIR}/downloaded.ckpt"
 
-if [[ ! -f "${MODEL_FILE}" ]]; then
+if [[ ! -f "${UNET_FILE}" ]]; then
   echo "Creating new session..."
   mkdir -p "${SESSION_DIR}"
-  rm -rf "${WORKING_MODEL_DIR}"
-  mkdir -p "${WORKING_MODEL_DIR}"
+  rm -rf "${SESSION_MODEL_DIR}"
+  mkdir -p "${SESSION_MODEL_DIR}"
   if [[ -z "${MODEL_PATH}" ]]; then
     echo "Using the default model..."
-    cp -r "/content/model" "${WORKING_MODEL_DIR}"
+    cp -r "/content/model" "${SESSION_MODEL_DIR}"
   elif [[ "${MODEL_PATH}" = "/"* ]]; then
     echo "Using model at ${MODEL_PATH}"
-    python3 -u /content/hf-diffusers/convert_original_stable_diffusion_to_diffusers.py --checkpoint_path "${MODEL_PATH}" --dump_path "${WORKING_MODEL_DIR}"
-  elif [[ "${MODEL_PATH}" = "https://"* ]]; then
+    python3 -u /content/hf-diffusers/convert_original_stable_diffusion_to_diffusers.py --checkpoint_path "${MODEL_PATH}" --dump_path "${SESSION_MODEL_DIR}"
+  elif [[ "${MODEL_PATH}" = "http"* ]]; then
     echo "Downloading model from ${MODEL_PATH}"
     rm -f "${MODEL_DOWNLOADED}"
-    wget -O "${MODEL_DOWNLOADED}" ${MODEL_PATH} || return 13
-    python3 -u /content/hf-diffusers/convert_original_stable_diffusion_to_diffusers.py --checkpoint_path "${MODEL_DOWNLOADED}" --dump_path "${WORKING_MODEL_DIR}"
+    wget -O "${MODEL_DOWNLOADED}" ${MODEL_PATH} || return 210
+    python3 -u /content/hf-diffusers/convert_original_stable_diffusion_to_diffusers.py --checkpoint_path "${MODEL_DOWNLOADED}" --dump_path "${SESSION_MODEL_DIR}"
     rm -f "${MODEL_DOWNLOADED}"
   fi
-  if [[ ! -f "${MODEL_FILE}" ]]; then
-    echo "Unable to find the base model!"
-    return 14
+  if [[ ! -f "${UNET_FILE}" ]]; then
+    echo "Unable to find the model!"
+    return 220
   fi
 else
   echo "Resuming previous session..."
 fi
 
 mkdir -p "${OUTPUT_DIR}"
+FINAL_CKPT="${SESSION_DIR}/${SESSION_NAME}.ckpt"
+rm -f "${FINAL_CKPT}"
 
+echo "Starting Dreambooth training..."
 !accelerate launch /content/diffusers/examples/dreambooth/train_dreambooth.py \
   --image_captions_filename
   --train_text_encoder
   --save_starting_step=500 \
   --stop_text_encoder_training=500 \
   --save_n_steps=500 \
-  --pretrained_model_name_or_path="${WORKING_MODEL_DIR}" \
+  --pretrained_model_name_or_path="${SESSION_MODEL_DIR}" \
   --instance_data_dir="${INSTANCE_DIR}" \
-  --output_dir="${OUTPUT_DIR}" \
-  --instance_prompt="${SESSION_NAME}" \
+  --output_dir="${SESSION_DIR}" \
+  --instance_prompt="${MODEL_NAME}" \
   --seed=1337 \
   --resolution=512 \
   --mixed_precision=fp16 \
@@ -61,5 +65,8 @@ mkdir -p "${OUTPUT_DIR}"
   --center_crop \
   --lr_warmup_steps=0 \
   --max_train_steps=2000
+
+echo "Saving final CKPT..."
+
 
 #exec "$@"
